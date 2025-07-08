4272279fcb27a13140d9b68f4147899fd208326c
Arush Sharma <arush-mac@Arushs-MacBook-Air.local>
2025-07-08 17:07:42 +0530

    solve n queen

--- 51.N-Queen.cpp
+++ 51.N-Queen.cpp
@@ -2,30 +2,43 @@
 #include<vector>
 using namespace std;
 
-bool isSafe(int rowIndex, int colIndex, vector<string> ds, string row){
+bool isSafe(vector<string> &board, int rowIndex, int colIndex, int n){
+    for(int i=0; i<n; i++) if(board[rowIndex][i] == 'Q') return false;
+    for(int i=0; i<n; i++) if(board[i][colIndex] == 'Q') return false;
+    for(int i =rowIndex, j=colIndex; i>=0 && j>=0; i--, j--) if(board[i][j] == 'Q') return false;
+    for(int i =rowIndex, j=colIndex; i>=0 && j<n; i--, j++) if(board[i][j] == 'Q') return false;
     return true;
 }
 
+void solve(vector<string> &board, int row, int n, vector<vector<string>> &ans) {
+    if(row == n) {
+        ans.push_back(board);
+        return;
+    }
 
-void solve(int rowIndex, int colIndex, vector<string> &ds, string row) {
-    row[colIndex] = 'Q';
-    ds.push_back(row);
+    for(int i=0; i<n; i++) {
+        if(isSafe(board, row, i, n)) {
+            board[row][i] = 'Q';
+            solve(board, row+1, n, ans);
+            board[row][i] = '.';
+        }
+    }
 }
 
 vector<vector<string>> solveNQueens(int n) {
     vector<vector<string>> ans;
-    vector<string> ds;
-    string row(n, '.');
-
-    for(int i=0; i<n; i++) {
-        solve(i, 0, ds, row);
-    }
+    vector<string> board(n, string(n, '.'));
+    solve(board, 0, n, ans);
+    return ans;
 }
 
 int main() {
-
     int n = 4;
-
     auto result = solveNQueens(n);
+    for(auto n: result) {
+        cout << "[";
+        for( auto s : n) cout << s << ", ";
+        cout << "]";
+    }
     return 0;
 }
